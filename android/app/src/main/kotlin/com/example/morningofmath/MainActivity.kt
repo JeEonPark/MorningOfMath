f4fdb83765e23926ce7bc8e5ee2a8927e4b7f4f4
JeEonPark <ggungtoo@icloud.com>
2021-11-08 20:28:23 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/morningofmath/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.morningofmath
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+  home: Home(),
+)); // 앱 실행 
+
+class Home extends StatelessWidget {
+  const Home({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    
+    appBar: AppBar(
+      title: const Text('my first app'),
+      centerTitle: true,
+      backgroundColor: Colors.red[600],
+    ),
+
+    body: Container(
+      padding: EdgeInsets.all(20.0),
+      color: Colors.grey[400],
+      child: Text('hello'),
+    ),
+
+    floatingActionButton: FloatingActionButton(
+      child: const Text('click'),
+      onPressed: () {},
+      backgroundColor: Colors.red[600],
+    ),
+  );
+  }
+}
